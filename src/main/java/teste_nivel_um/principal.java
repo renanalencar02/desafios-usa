535a8c32a313adc25d34d2485d8b66cd01dd0614
renanalencar02 <renanalencar02@gmail.com>
2023-10-27 09:25:26 -0300

    algoritmo que conta caracteres

---
+++ src/main/java/teste_nivel_um/principal.java
@@ -0,0 +1,33 @@
+package teste_nivel_um;
+
+import javax.print.DocFlavor;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class principal {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner read = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("----- SISTEMA QUE BUSCA E CONTA CARACTERES ESPECÍFICOS -----");
+        System.out.print("Digite um conjuto de caracteres: ");
+        String caracteres = read.nextLine();
+        System.out.print("Digite o caractere a ser buscado: ");
+        Character buscar = read.nextLine().charAt(0);
+
+        Integer total = ContarCaracteres(buscar.toString().toLowerCase(), caracteres.toLowerCase());
+        System.out.println("Total de caracteres buscados: "+ total);
+
+
+    }
+    public static Integer ContarCaracteres(String buscado, String conjuntoCaracteres){
+        int cont = 0;
+        for (int i = 0; i < conjuntoCaracteres.length(); i++) {
+            if (conjuntoCaracteres.charAt(i) == buscado.charAt(0)){
+               cont++;
+            }
+        }
+        return cont;
+    }
+}
