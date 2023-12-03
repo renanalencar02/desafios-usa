7db575f50cd285630e730a3b05234d2dbc1e36f8
Renan Alencar <renanalencar02@gmail.com>
2023-12-02 21:07:34 -0300

    corrigido problemas de entrada inválida

--- src/main/java/desafio_seis/Menu.java
+++ src/main/java/desafio_seis/Menu.java
@@ -1,26 +1,32 @@
 package desafio_seis;
 
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Menu {
+    private static Scanner ler = new Scanner(System.in);
     public static void abrirMenu() {
-        Scanner ler = new Scanner(System.in);
         Integer opcao;
-
         do {
-            try {
-                opcoesMenu();
-                opcao = ler.nextInt();
-                ler.nextLine();
-                switch (opcao) {
+               opcao = opcoesMenu();
+               switch (opcao) {
                     case 1:
                         System.out.print("Digite o nome da pessoa: ");
                         String nome = ler.nextLine();
                         System.out.print("Digite o email da pessoa: ");
                         String email = ler.nextLine();
                         System.out.print("Digite a idade da pessoa: ");
-                        Integer idade = ler.nextInt();
+                        Integer idade = null;
+                        while (idade == null){
+                            if(ler.hasNextInt()){
+                                idade = ler.nextInt();
+                                ler.nextLine();
+                            } else {
+                                System.out.print("Idade inválida. Digite apenas números: ");
+                                ler.nextLine();
+                            }
+                        }
                         System.out.println();
                         Pessoa pessoa = new Pessoa(nome, email, idade);
                         ServicoDeCadastro.cadastrarPessoa(pessoa);
@@ -39,23 +45,27 @@ public class Menu {
                         System.out.println("Opção inválida!");
                         System.out.println();
                 }
-
-            } catch(RuntimeException e){
-                System.out.println("Opção inválida!");
-                System.out.println();
-            } finally {
-                continue;
-            }
         }while (opcao != 0);
     }
 
-    public static void opcoesMenu() {
+    public static Integer opcoesMenu() {
+        Integer opcao = null;
         System.out.println("""
                 Olá, Usuário. Digite uma das opções abaixo:
                 1 - Cadastrar
                 2 - Listar
                 0 - Sair
                 """);
+        while (opcao == null) {
+            if (ler.hasNextInt()) {
+                opcao = ler.nextInt();
+                ler.nextLine();
+            } else {
+                System.out.println("Digite uma das opções válidas: 1, 2 ou 0");
+                ler.nextLine();
+            }
+        }
+        return opcao;
     }
 }
 
--- src/main/java/desafio_seis/Principal.java
+++ src/main/java/desafio_seis/Principal.java
@@ -1,7 +1,4 @@
 package desafio_seis;
-
-import java.util.Locale;
-
 public class Principal {
     public static void main(String[] args) {
         Menu.abrirMenu();
